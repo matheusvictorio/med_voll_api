5650706bb98ea1a6499e88398a51bac45542eb00
matheusvictorio <matheusvic2016@gmail.com>
2025-05-03 12:52:48 -0300

    First commit: Project setup and medico/paciente routes and Dtos

---
+++ src/main/java/med/voll/api/pacientes/DadosCadastroPaciente.java
@@ -0,0 +1,6 @@
+package med.voll.api.pacientes;
+
+import med.voll.api.endereco.DadosEndereco;
+
+public record DadosCadastroPaciente(String nome, String email, String telefone, String cpf, DadosEndereco endereco) {
+}
